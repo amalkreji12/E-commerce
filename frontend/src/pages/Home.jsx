e5ffaff709622d1418f7416e3862c4abbe7c2fcc
Amal K Reji <amalreji45.amal@gmail.com>
2025-03-04 16:07:52 +0530

    context added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import LatestCollection from '../components/LatestCollection/LatestCollection'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <LatestCollection/>
     </div>
   )
 }
