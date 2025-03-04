3e4b6c0e26a38ea63b5c90a69a22c5369e8093a8
Amal K Reji <amalreji45.amal@gmail.com>
2025-03-04 12:39:14 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/collection" exact element={<Collection />} />
+      <Route path="/about" exact element={<About />} />
+      <Route path="/contact" exact element={<Contact />} />
+      <Route path="/product/:productId" exact element={<Product />} />
+      <Route path="/cart" exact element={<Cart />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="place-order" exact element={<PlaceOrder />} />
+      <Route path="orders" exact element={<Orders />} />
+    </Routes>
+  </Router>
+);
 
 function App() {
   return (
-    <div className="bg-red-400">
-      <h1 class="text-3xl font-bold underline text-center">E-commerce</h1>
-    </div>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">{routes}</div>
   );
 }
 
