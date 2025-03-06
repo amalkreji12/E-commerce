6a92a40727bca3e7327cb49a8a8e9233f7ad911e
Amal K Reji <amalreji45.amal@gmail.com>
2025-03-06 14:58:51 +0530

    Policy section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero/Hero";
 import LatestCollection from "../components/LatestCollection/LatestCollection";
 import Bestseller from "../components/Bestseller/Bestseller";
+import Policy from "../components/Policy/Policy";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <LatestCollection />
       <Bestseller />
+      <Policy />
     </div>
   );
 };
