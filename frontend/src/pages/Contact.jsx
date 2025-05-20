ffc8568f719a90d7c2f2a4efaa0352379e89f88e
Amal K Reji <amalreji45.amal@gmail.com>
2025-05-20 15:46:54 +0530

    contact page added

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title/Title";
+import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox/NewsletterBox";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt=""
+        />
+
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            Akshya Nagar 1st Block 1st Cross,
+            <br /> Rammurthy nagar, Bangalore-560016
+          </p>
+          <p className="text-gray-500">
+            Tel: +91-123456789 <br /> Email: sample@gmail
+          </p>
+          <p className="font-semibold text-xl text-gray-600">Careers @ Vivid</p>
+          <p className="text-gray-500">
+            If you are interested in joining our team, please send your resume
+            to <br />
+            <a href="mailto:sample@gmail">sample@gmail</a>
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+
+      <NewsletterBox />
+      
+    </div>
+  );
+};
+
+export default Contact;
