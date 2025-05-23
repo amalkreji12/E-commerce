57653f7b3fa844eb522ce08a6a3a45da18179b99
Amal K Reji <amalreji45.amal@gmail.com>
2025-05-23 15:47:17 +0530

    backend setup

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=> {
+    res.send('Hello World');
+});
+
+app.listen(port, () => console.log('Server is running on port ' + port));
