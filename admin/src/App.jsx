a121ed9c43e6bbf59d306e4c82f9ec77b3c2bcf1
Amal K Reji <amalreji45.amal@gmail.com>
2025-06-02 12:44:49 +0530

    admin panel frontend

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./conponents/NavBar";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default App;
---
+++ admin/src/pages/listProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const listProduct = () => {
+  return (
+    <div>listProduct</div>
+  )
+}
+
+export default listProduct
